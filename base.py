b4d7a271e4b33d72e1a62ecbef17aed1884f0257
Fusuke Minagi <fussinboots22@gmail.com>
2024-07-17 17:21:38 +0900

    init

---
+++ base.py
@@ -0,0 +1,28 @@
+import kachaka_api
+import matplotlib.pyplot as plt
+import numpy as np
+# from IPython.display import Image, display
+# ip changes everytime check with kachacar
+KACHAKA_HOST = "192.168.118.79"
+# grpc port
+KACHAKA_PORT = 26400
+
+
+def main():
+    client = kachaka_api.KachakaApiClient("%s:%s" % (KACHAKA_HOST, KACHAKA_PORT))
+    client.set_manual_control_enabled(True) # If true only moves with command
+
+    # main velocity
+    
+    scan = client.get_ros_laser_scan()
+
+    theta = np.linspace(scan.angle_min, scan.angle_max, len(scan.ranges))
+    dist = np.array(scan.ranges)
+
+    # LiDARの点群を表示するサンプル
+    plt.scatter(dist * np.cos(theta), dist * np.sin(theta))
+    plt.savefig("test.png")
+
+
+if __name__ == "__main__":
+    main()
